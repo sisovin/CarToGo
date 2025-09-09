210377e2e05b18906a6654851d98d5c7b0accc6e
Your Name <you@example.com>
2025-09-09 18:54:41 +0700

    Fixing onSwitchToSignup={() => setAuthMode(signup)}

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -333,7 +333,7 @@ export default function Home() {
                     {/* Direct Login Form */}
                     <RiderSignin
                       onSignin={handleSignedIn}
-                      onSwitchToSignup={() => setIsSignup(true)}
+                      onSwitchToSignup={() => setAuthMode("signup")}
                       initialRole="admin"
                     />
                   </CardContent>
